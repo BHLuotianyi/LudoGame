7a3d0e5f0404ae338a8df12a3e22b3aa37d7e6f3
Ramtyn <ramtincv@gmail.com>
2026-05-12 10:10:44 +0800

    feat: change The Dice

--- Dice.java
+++ Dice.java
@@ -1,21 +1,17 @@
-/** 
- * Dice.java
- * To deal with the random number formatting.
+/**
+ * Dice class for the Ludo project.
+ * Implements only the green getter as per UML specifications.
  */
 public class Dice {
     private int result;
 
-    //  CONSTRUCTOR 
-    public Dice() {
-        // Initialize the dice result to a default value (e.g., 0 or 1)
-        this.result = 1;
-    }
-
     /**
-     * Gets the last rolled dice result.
-     * @return The current value of the dice (1-6).
+     * Generates and returns a random dice roll between 1 and 6.
+     * This acts as the primary method to get a move value.
      */
     public int getResult() {
+        // Generates a random number from 1 to 6
+        this.result = (int) (Math.random() * 6) + 1;
         return this.result;
     }
 }

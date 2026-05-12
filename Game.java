f052f08bf636292f330a0f32a2e748357207b6f2
BHLuotianyi <2356747603@qq.com>
2026-05-12 21:54:24 +0800

    fix: getColorById may trigger nullPointerException

--- Game.java
+++ Game.java
@@ -223,11 +223,11 @@ public class Game {
      * @return the player's color, or null if the player is not found
      */
     public String getColorById(int id) {
-        String result = getPlayerById(id).getColor();
-        if (result == null) { // Avoid null pointer exception
+        if (getPlayerById(id).getColor() == null) { // Avoid null pointer exception
             return null;
+        } else {
+            return getPlayerById(id).getColor();
         }
-        return result;
     }
 
     /**

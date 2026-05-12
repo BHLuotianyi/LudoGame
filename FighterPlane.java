9af174ea18ce59db96f30a43c964b7f10b2068bd
BHLuotianyi <2356747603@qq.com>
2026-05-12 10:09:00 +0800

    feat: add name setting to plane classes

--- FighterPlane.java
+++ FighterPlane.java
@@ -3,7 +3,7 @@
  * Represents a low-level plane in the Ludo game.
  */
 public class FighterPlane extends Plane {
-    public FighterPlane(Player owner, int id) {
-        super(owner, 1, id);
+    public FighterPlane(Player owner, int id, String name) {
+        super(owner, 1, id, name);
     }
 }

a28a3cab2866bc90ca0d1074a3242b517ab3220c
BHLuotianyi <2356747603@qq.com>
2026-05-21 03:00:03 +0800

    feat: implement the program entry

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@
  * Main.java
  * The entry point of the Ludo game application. This class contains the main method which initializes and starts the game.
  */
-class Main {
+public class Main {
     public static void main(String[] args) {
-        // Sample comment
-        // d
+        GameController controller = new GameController();
+        controller.play();
     }
 }
